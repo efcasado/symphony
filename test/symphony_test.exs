d880f76a930f78028bbe590e75f25ab09335af77
Enrique Fernandez <efcasado@gmail.com>
2016-08-21 16:45:34 +0200

    Ensure app is using the default values

--- test/symphony_test.exs
+++ test/symphony_test.exs
@@ -1,9 +1,10 @@
 defmodule SymphonyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Symphony
 
   setup do
     Application.stop(:symphony)
+    Application.unload(:symphony)
     :ok = Application.start(:symphony)
   end
 
